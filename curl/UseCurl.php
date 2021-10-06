52c62c108c0e5c3d2f7bb84fca29693ae854231d
Nazarychev_905 <vladnaz9@mail.ru>
2021-10-06 16:33:47 +0300

    curl home work

---
+++ curl/UseCurl.php
@@ -0,0 +1,38 @@
+<?php
+
+if (isset($_REQUEST["string"])) {
+    $string = $_POST['string'];
+    $fio = $_POST['FIO'];
+
+
+    $url = "https://reqbin.com/echo/post/json";
+//    $data = explode(PHP_EOL, $string);
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $arr = ["esse"=> $string, 'fio' => $fio];
+
+    $data = json_encode($arr);
+//    var_dump($data);
+    $headers = array(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    );
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+    $resp = curl_exec($curl);
+    curl_close($curl);
+//var_dump($resp);
+    if ($resp == True) {
+        echo "Успешно отправлено!";
+    } else echo " ошибка отправления ";
+}
+?>
+
