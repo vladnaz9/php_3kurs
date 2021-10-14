ac8409da346ac490a00c286cdde5b225b166c2c1
Nazarychev_905 <vladnaz9@mail.ru>
2021-10-14 12:53:06 +0300

    add form for howe work2

--- app/index.php
+++ app/index.php
@@ -1,99 +1,110 @@
 <?php
 
-require_once "php_3kurs/vendor/autoload.php";
-spl_autoload_register(function ($className) {
-    require_once 'php_3kurs/app/classes/' . $className . '.inc';
-});
+if (isset($_REQUEST["url"])) {
+    $url = $_POST['url'];
+    $deep = $_POST['deep'];
+    require_once "php_3kurs/vendor/autoload.php";
+    spl_autoload_register(function ($className) {
+        require_once 'php_3kurs/app/classes/' . $className . '.inc';
+    });
+
+    function parse(UriDB $uridb, int $parentId, int $deep = 2)
+    {
+        if ($deep == 1) {
+            return;
+        }
+        $uris = $uridb->QueryById($parentId);
+        foreach ($uris as $newUri) {
+            $url = $newUri->getUri();
+
+            $client = new GuzzleClient($url);
+            try {
+                $content = $client->get($url);
+            } catch (UnsuccessfulRequestException $ure) {
+                continue;
+                throw new Exception($ure->getMessage() . " Code: " . $ure->getCode() . " Http message: " . $ure->getHttpReason());
+            } catch (NotInitializedException $nie) {
+                continue;
+                throw new Exception("Client not initialized for some mysterious reason");
+            }
+
+
+            $parser = new UriParser($content);
+
+            $result1 = $parser->getResult();
+            $parentId = $newUri->getId();
+
+            foreach ($result1 as $key => $item) {
+                if (substr($item, 0, 1) === "#") {
+                    unset($result1[$key]);
+                    continue;
+                }
+                if (substr($item, 0, 1) === "/") {
+                    unset($result1[$key]);
+                    continue;
+                }
+//
+            }
+
+            foreach ($result1 as $uri) {
+                $uridb->Add(new Uri($uri, null, $parentId));
+                parse($uridb, $parentId, $deep--);
+            }
+        }
+    }
 
 //$url = "http://drs.ua/rus/registrars.html";
-$url = "http://www.abc.edu-net.khb.ru/";
-$client = new GuzzleClient($url);
-try {
-    $content = $client->get($url);
-} catch (UnsuccessfulRequestException $ure) {
-    throw new Exception($ure->getMessage() . " Code: " . $ure->getCode() . " Http message: " . $ure->getHttpReason());
-} catch (NotInitializedException $nie) {
-    throw new Exception("Client not initialized for some mysterious reason");
-}
+//$url = "http://www.abc.edu-net.khb.ru/";
 
-$parser = new UriParser($content);
-$result = $parser->getResult();
-
-foreach ($result as $key => $item) {
-    if (substr($item, 0, 1) === "#") {
-        unset($result[$key]);
-        continue;
+    $client = new GuzzleClient($url);
+    try {
+        $content = $client->get($url);
+    } catch (UnsuccessfulRequestException $ure) {
+        throw new Exception($ure->getMessage() . " Code: " . $ure->getCode() . " Http message: " . $ure->getHttpReason());
+    } catch (NotInitializedException $nie) {
+        throw new Exception("Client not initialized for some mysterious reason");
     }
-    if (substr($item, 0, 1) === "/") {
-        unset($result[$key]);
-        continue;
-    }
-}
 
-$host = "localhost";
-$port = 5432;
-$dbname = "Url_parser";
-$user = "postgres";
-$password = "root";
-$uridb = new UriDB($host, $port, $dbname, $user, $password);
+    $parser = new UriParser($content);
+    $result = $parser->getResult();
 
-
-$parent = $uridb->Add(new Uri($url));
-
-foreach ($result as $uri) {
-    $uridb->Add(new Uri($uri, null, $parent));
-}
-
-parse($uridb, $parent);
-
-$dbresult = $uridb->GetAllUris();
-
-
-echo Renderer::render('uris', ['uris' => $dbresult]);
-
-function parse(UriDB $uridb, int $parentId, int $deep = 2)
-{
-    if ($deep == 1) {
-        return;
-    }
-    $uris = $uridb->QueryById($parentId);
-    foreach ($uris as $newUri) {
-        $url = $newUri->getUri();
-
-        $client = new GuzzleClient($url);
-        try {
-            $content = $client->get($url);
-        } catch (UnsuccessfulRequestException $ure) {
+    foreach ($result as $key => $item) {
+        if (substr($item, 0, 1) === "#") {
+            unset($result[$key]);
             continue;
-            throw new Exception($ure->getMessage() . " Code: " . $ure->getCode() . " Http message: " . $ure->getHttpReason());
-        } catch (NotInitializedException $nie) {
+        }
+        if (substr($item, 0, 1) === "/") {
+            unset($result[$key]);
             continue;
-            throw new Exception("Client not initialized for some mysterious reason");
-        }
-
-
-        $parser = new UriParser($content);
-
-        $result1 = $parser->getResult();
-        $parentId = $newUri->getId();
-
-        foreach ($result1 as $key => $item) {
-            if (substr($item, 0, 1) === "#") {
-                unset($result1[$key]);
-                continue;
-            }
-            if (substr($item, 0, 1) === "/") {
-                unset($result1[$key]);
-                continue;
-            }
-//
-        }
-
-        foreach ($result1 as $uri) {
-            $uridb->Add(new Uri($uri, null, $parentId));
-            parse($uridb, $parentId, $deep--);
         }
     }
+
+    $host = "localhost";
+    $port = 5432;
+    $dbname = "Url_parser";
+    $user = "postgres";
+    $password = "root";
+    $uridb = new UriDB($host, $port, $dbname, $user, $password);
+
+
+    $parent = $uridb->Add(new Uri($url));
+
+    foreach ($result as $uri) {
+        $uridb->Add(new Uri($uri, null, $parent));
+    }
+
+    try {
+        parse($uridb, $parent, $deep);
+    } catch (Exception $e) {
+        echo "end";
+    }
+
+    $dbresult = $uridb->GetAllUris();
+
+
+    echo Renderer::render('uris', ['uris' => $dbresult]);
+
+
 }
 
 
